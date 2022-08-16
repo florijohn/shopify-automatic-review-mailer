cfa576d499bed90abf686ea508203b7d063bcc8a
Floris <65296458+florijohn@users.noreply.github.com>
2022-08-16 21:50:57 +0200

    deleted comments

--- public/sendReviewMail.php
+++ public/sendReviewMail.php
@@ -1,30 +1,6 @@
 <?php
 require_once '../config/config.php';
 require_once '../assets/functions.php';
-// TODO Daten aus DB Abfragen
-/*
-    Done Daten aus DB Abfragen
-    Done doppelte Einträge zählen
-    done emails bei denen doppelte Einträge gezählt werden in Mail array schreiben
-    Mail array durchlaufen und functiion sendMail mit Email und Template aufrufen
-    function sendmail umbauen auf Übergabeparameter: Email, Template
-    bei Bestellungen = 2 senden einer email
-    Cronjob einrichten
-
-
-    SQL Abfrage
-
-    Alle Mails bei denen die Bestellungen 2 sind
-
-    SELECT fullName AS kunde, COUNT(mail) AS Anzahl, mail AS EMail FROM customers GROUP BY mail HAVING (COUNT(mail) > 1);
-
-
-    TODO Datum Anfragen und Mail nur schicken wenn letzte Mail 30 Tage her ist und Bedingung erfüllt ist
-
-    Bedingung: isHappy = 0
-    lastSent: 30 Tage her
-*/
-
 
 // create DB Connection
 $conn = new mysqli($servername, $username, $password, $dbname);
