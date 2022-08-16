0ca267594ab67a679dd685349c7cb045fa1c04e2
Floris <65296458+florijohn@users.noreply.github.com>
2022-08-16 21:42:36 +0200

    add condition to send emails only when customer unhaappy and last email 30 days ago

--- public/sendReviewMail.php
+++ public/sendReviewMail.php
@@ -33,21 +33,26 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT fullName AS kunde, COUNT(mail) AS Anzahl, mail AS EMail FROM customers GROUP BY mail HAVING (COUNT(mail) > 1)";
+$sql = "SELECT fullName AS kunde, COUNT(mail) AS Anzahl, mail AS EMail, ishappy, lastMailSent FROM customers GROUP BY mail HAVING (COUNT(mail) > 1)";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     $emails[] = $row["EMail"];
+    $isHappy[] = $row["ishappy"];
+    $lastMailSent[] = $row["lastMailSent"];
   }
 } else {
   echo "0 results";
 }
 
 for($i = 0; $i < count($emails); $i++) {
-  sendEmail($emails[$i]);
-  updateDB($emails[$i]);
+  if($isHappy[$i] == 0 && $lastMailSent[$i] < date("Y-m-d", strtotime("-30 days"))) {
+    sendEmail($emails[$i]);
+  } else {
+    echo "Keine Mail gesendet";
+  }
 }
 
 $conn->close();
